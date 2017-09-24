6b6d217ce9824f13a08f860bda9a2eb1c1d0d6f2
Jonathan Kammo <jonathankammo@gmail.com>
2017-09-23 21:59:48 -0500

    Initialize project and create functioning expo/reactNative app boilerplate

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+/* eslint-disable react/prefer-stateless-function, class-methods-use-this */
+export default class App extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={styles.titleText}>
+          {'\n'}Jon's Nest
+        </Text>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    // fontFamily: 'Serif',
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
